ac140c1abf9ef5f9aa66b3c2cdad34e9a0ff91aa
Chris Rehn <crehn@outlook.com>
2020-07-26 12:57:37 -0700

    Refactor cache

--- cache/cache.go
+++ cache/cache.go
@@ -17,18 +17,18 @@ func New(size int) *Cache {
 }
 
 func (c *Cache) evict() {
-	if len(c.data) >= c.size {
-		for k := range c.data {
-			delete(c.data, k)
-			break
-		}
+	for k := range c.data {
+		delete(c.data, k)
+		return
 	}
 }
 
 func (c *Cache) Put(k string, b []byte) {
 	c.Lock()
 	defer c.Unlock()
-	c.evict()
+	if len(c.data) >= c.size {
+		c.evict()
+	}
 	c.data[k] = b
 }
 
